812919b65dcec4894d2b8eb39242b2e2457ca6f4
Albert Brown <103575236+Albert-7317@users.noreply.github.com>
2022-07-26 10:27:09 +0000

    Update weather_scrapper.py

--- weather_scrapper.py
+++ weather_scrapper.py
@@ -12,20 +12,29 @@ current_temp = result_temp.find("span", {"class":"wr-hide-visually"}).text
 
 current_condition = soup.find("div", {"class":"wr-weather-type__text"}).text
 
-print(current_condition)
-print(current_temimport requests
-from bs4 import BeautifulSoup
+result_location = soup.find("div", {"id":"wr-forecast"})
 
-URL = 'https://www.bbc.co.uk/weather/2643743'
-page = requests.get(URL)
+current_location = result_location.find("h1", {"id":"wr-location-name-id"}).text
 
-soup = BeautifulSoup(page.content, 'html.parser')
-
-result_temp = soup.find("div", {"class":"wr-value--temperature gel-trafalgar"})
-
-current_temp = result_temp.find("span", {"class":"wr-hide-visually"}).text
-
-current_condition = soup.find("div", {"class":"wr-weather-type__text"}).text
-
-print(current_condition)
-print(current_tem
+print("your cuurent location is ",current_location)
+print("the current conditions are ",current_condition)
+print("and the current temprature is ",current_temp)
+print("          ,$Y$$$$Y$")
+print("         $$,   ,+$P,")
+print("        $,      $Y,")
+print("        Y,                 $$,")
+print("      , ,Y,                $Y+%PYYY$$$$$Y,,")
+print("    ,$Y$..$$,,,,  ,,,$+Y$$$$$Y%+,,,   ,,+P$$$$,")
+print("    ,Y,,   ,,,$YYYYY$$YPY,,,            ,$Y  ,$+Y$,")
+print("     ,,$$$,,,,,,        ,,$$$$$$,         ,  ,,+$,$$$")
+print("                              ,$Y$,,           $$  ,YY")
+print("                                 ,$$$$$,       $,  ,Y*,")
+print("                                     ,,$Y$Y$       $Y,+,")
+print("                                         ,,Y$,     , ,%,")
+print("                                          $Y++,     ,YP$")
+print("                                         ,PP+$$,,   ,,+,")
+print("                                         ,YPPY+Y$,  ,+Y")
+print("                                 YPP,,     $,$+,    $+")
+print("                                 YYP$++,   ,$Y$,  ,$+,")
+print("                                  $$Y,Y+Y$$$$$, ,$Y$")
+print("                                   ,$$,,$$$,,,$$,,")
